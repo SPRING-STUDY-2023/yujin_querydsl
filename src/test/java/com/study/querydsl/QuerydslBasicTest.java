eb61c2954026c496c7c0233099886498b6138487
yujindonut <bbb3631@naver.com>
2023-03-23 20:38:54 +0900

    feat: 정렬

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -1,7 +1,10 @@
 package com.study.querydsl;
 
-import static com.mysema.commons.lang.Assert.assertThat;
+import static com.querydsl.core.types.ExpressionUtils.isNull;
 import static com.study.querydsl.Entity.QMember.member;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.querydsl.core.QueryResults;
 import java.util.*;
@@ -12,6 +15,7 @@ import com.study.querydsl.Entity.Team;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,9 +23,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @Transactional
 public class QuerydslBasicTest {
+
     @PersistenceContext
     EntityManager em;
     JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
     @BeforeEach
     public void before() {
         Team teamA = new Team("teamA");
@@ -37,14 +43,16 @@ public class QuerydslBasicTest {
         em.persist(member3);
         em.persist(member4);
     }
+
     @Test
     public void startJPQL() {
-    String qlString = "select m from Member m " + "where m.username = :username";
+        String qlString = "select m from Member m " + "where m.username = :username";
         Member findMember = em.createQuery(qlString, Member.class)
                 .setParameter("username", "member1")
                 .getSingleResult();
-//        assert (findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername(), is(equalTo("member1")));
     }
+
     @Test
     public void startQuerydsl() {
 //member1을 찾아라.
@@ -54,8 +62,9 @@ public class QuerydslBasicTest {
                 .from(m)
                 .where(m.username.eq("member1"))//파라미터 바인딩 처리
                 .fetchOne();
-//        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername(),is(equalTo("member1")));
     }
+
     @Test
     public void startQuerydsl2() {
         QMember m = new QMember("m");
@@ -64,7 +73,7 @@ public class QuerydslBasicTest {
                 .from(m)
                 .where(m.username.eq("member1"))
                 .fetchOne();
-//        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername(),is(equalTo("member1")));
     }
 
     @Test
@@ -78,8 +87,9 @@ public class QuerydslBasicTest {
                 .from(member)
                 .where(member.username.eq("member1"))
                 .fetchOne();
-//        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername(),is(equalTo("member1")));
     }
+
     @Test
     public void search() {
         Member findMember = queryFactory
@@ -87,7 +97,7 @@ public class QuerydslBasicTest {
                 .where(member.username.eq("member1")
                         .and(member.age.eq(10)))
                 .fetchOne();
-//        assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername(),is(equalTo("member1")));
     }
 
     @Test
@@ -97,7 +107,7 @@ public class QuerydslBasicTest {
                 .where(member.username.eq("member1"),
                         member.age.eq(10))
                 .fetch();
-//        assertThat(result1.size()).isEqualTo(1);
+        assertThat(result1.size(), is(equalTo(1)));
     }
 
     @Test
@@ -122,6 +132,31 @@ public class QuerydslBasicTest {
         long count = queryFactory
                 .selectFrom(member)
                 .fetchCount();
+    }
+
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername(), is(equalTo("member5")));
+        assertThat(member6.getUsername(), is(equalTo("member6")));
     }
 }
 
