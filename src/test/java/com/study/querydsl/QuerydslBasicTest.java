b1b5a3751c2b15ca8b0db2927cdd366ef0a74181
yujindonut <bbb3631@naver.com>
2023-03-23 20:22:12 +0900

    feat: 기본문법 - 시작 & 기본 Q타입 활용

---
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,83 @@
+package com.study.querydsl;
+
+import static com.mysema.commons.lang.Assert.assertThat;
+import static com.study.querydsl.Entity.QMember.member;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.Entity.Member;
+import com.study.querydsl.Entity.QMember;
+import com.study.querydsl.Entity.Team;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @PersistenceContext
+    EntityManager em;
+    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+    @Test
+    public void startJPQL() {
+    String qlString = "select m from Member m " + "where m.username = :username";
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+//        assert (findMember.getUsername()).isEqualTo("member1");
+    }
+    @Test
+    public void startQuerydsl() {
+//member1을 찾아라.
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))//파라미터 바인딩 처리
+                .fetchOne();
+//        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+    @Test
+    public void startQuerydsl2() {
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+//        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl3() {
+
+        QMember qMember = new QMember("m"); //별칭 직접 지정 // 같은 테이블 조인할때
+        QMember qMember1 = QMember.member; //기본 인스턴스 사용
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+//        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+}
+
