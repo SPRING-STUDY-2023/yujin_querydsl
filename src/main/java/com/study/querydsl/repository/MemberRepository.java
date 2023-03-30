8d2b9ec88a5e08118841769e48dca099cc620d6e
yujindonut <bbb3631@naver.com>
2023-03-30 21:06:19 +0900

    feat: QuerydslRepositorySupport

--- src/main/java/com/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface MemberRepository extends JpaRepository<Member, Long> ,
+        QuerydslPredicateExecutor<Member> {
     List<Member> findByUsername(String username);
 
     Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
 
     Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
+
 }
