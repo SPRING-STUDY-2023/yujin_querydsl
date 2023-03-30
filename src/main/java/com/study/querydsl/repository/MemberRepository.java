14ba425a0e7bcc7439243aa07dd7e7b8f41891a0
yujindonut <bbb3631@naver.com>
2023-03-30 20:57:55 +0900

    feat: 컨트롤러 개발

--- src/main/java/com/study/querydsl/repository/MemberRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -2,8 +2,16 @@ package com.study.querydsl.repository;
 
 
 import com.study.querydsl.Entity.Member;
+import com.study.querydsl.Entity.MemberSearchCondition;
+import com.study.querydsl.Entity.dto.MemberTeamDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsername(String username);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
