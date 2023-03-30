807988232f2fe12081b1d0912b88200e96140a32
yujindonut <bbb3631@naver.com>
2023-03-30 20:24:04 +0900

    feat: 스프링 데이터 JPA 리포지토리로 변경

---
+++ src/main/java/com/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.study.querydsl.repository;
+
+
+import com.study.querydsl.Entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
+}
