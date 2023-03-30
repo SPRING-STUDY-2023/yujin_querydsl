f20853e539fbe95faa8887b6257a6b19d1b2a455
yujindonut <bbb3631@naver.com>
2023-03-30 20:21:53 +0900

    feat: 조회 API 컨트롤러 개발

--- src/main/java/com/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/com/study/querydsl/repository/MemberJpaRepository.java
@@ -1,7 +1,15 @@
 package com.study.querydsl.repository;
 
+import static com.study.querydsl.Entity.QMember.member;
+import static com.study.querydsl.Entity.QTeam.team;
+import static org.apache.logging.log4j.util.Strings.isEmpty;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.Entity.Member;
+import com.study.querydsl.Entity.MemberSearchCondition;
+import com.study.querydsl.Entity.dto.MemberTeamDto;
+import com.study.querydsl.Entity.dto.QMemberTeamDto;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
@@ -30,4 +38,42 @@ public class MemberJpaRepository {
         return em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
-    } }
+    }
+
+
+    //회원명, 팀명, 나이(ageGoe, ageLoe)
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+    public List<Member> findMember(MemberSearchCondition condition) {
+        return queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+}
--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -682,41 +682,6 @@ public class QuerydslBasicTest {
 //        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
 //        assertThat(result).extracting("username").containsExactly("member4");
     }
-
-    //회원명, 팀명, 나이(ageGoe, ageLoe)
-    public List<MemberTeamDto> search(MemberSearchCondition condition) {
-        return queryFactory
-                .select(new QMemberTeamDto(
-                        member.id,
-                        member.username,
-                        member.age,
-                        team.id,
-                        team.name))
-                .from(member)
-                .leftJoin(member.team, team)
-                .where(teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe()))
-                .fetch();
-    }
-    private BooleanExpression teamNameEq(String teamName) {
-        return isEmpty(teamName) ? null : team.name.eq(teamName);
-    }
-    private BooleanExpression ageGoe(Integer ageGoe) {
-        return ageGoe == null ? null : member.age.goe(ageGoe);
-    }
-    private BooleanExpression ageLoe(Integer ageLoe) {
-        return ageLoe == null ? null : member.age.loe(ageLoe);
-    }
-    public List<Member> findMember(MemberSearchCondition condition) {
-        return queryFactory
-                .selectFrom(member)
-                .leftJoin(member.team, team)
-                .where(teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe()))
-                .fetch();
-    }
 }
 
 
